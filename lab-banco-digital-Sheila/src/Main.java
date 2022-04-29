d9306504c48603f52c30dcb156e0ff3751b3ef45
Sheila Calixto <99204813+sheilamcalixto@users.noreply.github.com>
2022-04-29 18:18:18 -0300

    Add files via upload
    Atualização e inclusão do html e css, dados processados com o banco de dado local, MSQL.

---
+++ lab-banco-digital-Sheila/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+		cc.depositar(100);
+		cc.transferir(80, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+
+
+	    
+	}
+
+}
